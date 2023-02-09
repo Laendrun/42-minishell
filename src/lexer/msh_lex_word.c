0e0421b33440c4b7e1ceffd5ee2ebc107715d46f
Simon Aeby <saeby@student.42lausanne.ch>
2023-02-09 16:28:41 +0100

    modified msh_lex_word to work with new lexer

--- src/lexer/msh_lex_word.c
+++ src/lexer/msh_lex_word.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   msh_lex_word.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saeby <saeby>                              +#+  +:+       +#+        */
+/*   By: saeby <saeby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:50:11 by saeby             #+#    #+#             */
-/*   Updated: 2023/01/28 10:32:17 by saeby            ###   ########.fr       */
+/*   Updated: 2023/02/09 16:26:30 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	msh_lex_word(t_msh_data *m_data, char *line, unsigned int *i)
+/*int	msh_lex_word(t_msh_data *m_data, char *line, unsigned int *i)
 {
 	unsigned int	t;
 	char			*tmp;
@@ -22,6 +22,21 @@ int	msh_lex_word(t_msh_data *m_data, char *line, unsigned int *i)
 		*i += 1;
 	tmp = ft_substr(line, t, *i - t);
 	msh_tok_lstaddb(&m_data->tokens, msh_tok_lstnew(WORD, ft_strdup(tmp)));
+	free(tmp);
+	return (SUCCESS);
+}*/
+
+int	msh_lex_word(t_msh_data *m_d, char *line, unsigned int *i)
+{
+	unsigned int	t;
+	char			*tmp;
+
+	t = *i;
+	while (!msh_isspace(line[*i]) && line[*i]
+		&& line[*i] != '|' && line[*i] != '<' && line[*i] != '>')
+		*i += 1;
+	tmp = ft_substr(line, t, *i - t);
+	msh_tok_lstaddb(&m_d->tokens, msh_tok_lstnew(WORD, ft_strdup(tmp)));
 	free(tmp);
 	return (SUCCESS);
 }
