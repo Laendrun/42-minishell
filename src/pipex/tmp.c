5518dca39c0d439a0d52c296bae1964c3a97f753
Elie Gauthey <egauthey@c2r4s2.42lausanne.ch>
2023-02-15 17:28:11 +0100

    tweaks in hdoc

--- src/pipex/tmp.c
+++ src/pipex/tmp.c
@@ -144,7 +144,7 @@ char	*pip_get_exec(char *cmd, char **paths)
 {
 	char	*path;
 
-	if ((ft_strncmp(cmd, "./", 2) == 0)
+	if ((ft_strncmp(cmd, "/", 1) == 0)
 		&& access(cmd, X_OK) == 0)
 		return (ft_strdup(cmd));
 	if ((ft_strncmp(cmd, "./", 2) == 0)
